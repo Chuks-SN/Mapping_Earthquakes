2c65c2cb7ed25f4fd10d0f5e1dd9a93d33460c51
Chuks Nwokedi <chuksnwokedi@gmail.com>
2022-02-26 10:52:19 -0500

    Update

--- Simple_Map/static/js/logicStep1.js
+++ Simple_Map/static/js/logicStep1.js
@@ -1,8 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 //  Add a marker to the map for Los Angeles, California.
 L.circleMarker([34.0522, -118.2437], {
@@ -17,6 +15,27 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     maxZoom: 18,
     accessToken: API_KEY
 });
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    "Streets": streets,
+    "Satellite": satelliteStreets
+  };
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zomm: 3,
+    layeres: [streets]
+
+});
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
+
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
